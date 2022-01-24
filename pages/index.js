7e6a14a77650f3c3437a85ce2f85e852045d2e5e
Heneros <punk60826@gmail.com>
2022-01-24 22:50:46 +0200

    new component Property created

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import Property  from "../components/Property";
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 
@@ -47,7 +48,7 @@ export default function Home({propertiesForSale, propertiesForRent}) {
  imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
       <Flex flexWrap="wrap">
-
+          {propertiesForRent.map((property) => <Property key={property.id} /> )}
       </Flex>
       <Banner 
    purpose='BUY A HOME'
@@ -60,7 +61,7 @@ export default function Home({propertiesForSale, propertiesForRent}) {
    imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
       <Flex flexWrap="wrap">
-
+      {propertiesForSale.map((property) => <Property key={property.id} /> )}
       </Flex>
       </div>
   )
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -9,6 +9,6 @@ export const fetchApi = async (url) =>{
             'x-rapidapi-key': ''
           },
     });
- 
+
     return data;
 }
