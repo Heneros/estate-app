99a791e2002462628ebbacfbf153b9b28747f0fb
Heneros <punk60826@gmail.com>
2022-01-28 01:34:40 +0200

    display filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,11 +4,30 @@ import { useRouter} from 'next/router';
 import { MDCancel } from 'react-icons/md';
 import Image from 'next/image';
 
+import { filterData, getFilterValues } from '../utils/FilterData';
+
+
 const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filrerValues) => {
+
+    }
+
     return (
-        <div>
-            SearchFilters
-        </div>
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map( (filter) =>(
+                <Box key={filter.queryName}>
+                    <Select 
+                    placeholder={filter.placeholder}
+                    w="fit-content"
+                    p="2"
+                    onChange={(e) => searchProperties({[filter.queryName] : e.target.value })}>
+
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
     )
 }
 
