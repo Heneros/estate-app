4d964979755ba52f6333ac13e7a78da8a95e35f5
Heneros <punk60826@gmail.com>
2022-01-31 02:30:09 +0200

    display properties in item

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -3,14 +3,76 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
+import { Avatar } from '@chakra-ui/avatar';
 
 import ImageScrollbar from '../../components/ImageScrollbar';
 
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 
-const PropertyDetails = ({propertyDetails: {price, rentFrquency, rooms, title, baths, area, agency, isVerifid, description, type, purpose, furnishingStatus, amenities, photos}}) => (
+const PropertyDetails = ({propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerifid, description, type, purpose, furnishingStatus, amenities, photos}}) => (
   <Box maxWidth="1000px" margin="auto" p="4">
    {photos && <ImageScrollbar data={photos} />}
+   <Box w="full" p="6">
+   <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                   <Box paddingRight="3" color="green.400">
+           
+                       <Text fontWeight="bold" fontSize="lg">
+                           AED {millify(price)} {rentFrequency && `/${rentFrequency}`}
+                       </Text>
+                   </Box>
+               </Flex>
+               <Box>
+                   <Avatar size="sm" src={agency?.logo?.url} />
+               </Box>
+               <Flex alignItems="center" p="1" justifyContent="space-between"
+               w="250px" color="blue.400">
+                   {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+               </Flex>
+               <Box marginTop="2">
+               <Text fontSize="lg" marginBottom="2" fontWeight="bold">
+                   {title}
+               </Text>
+               <Text lineHeight="2" color="gray.600">
+               {description.length > 200 ? `${description.substring(0, 200)}...` : description }
+               </Text>
+               </Box>
+               <Flex flexWrap="wrap" textTransform="uppercase" justifyContent="space-between">
+                   <Flex justifyContent="space-between" w="200px" borderBottom="1px" borderColor="gray.100"
+                   p="3">
+                       <Text>Type</Text>
+                       <Text fontWeight="bold">{type}</Text>
+                   </Flex>
+                   <Flex justifyContent="space-between" w="400px" borderBottom="1px" borderColor="gray.100"
+                   p="3">
+                       <Text>Purpose</Text>
+                       <Text fontWeight="bold">{purpose}</Text>
+                   </Flex>
+                   {furnishingStatus && (
+                    <Flex justifyContent="space-between" w="400px" borderBottom="1px" borderColor="gray.100"
+                    p="3">
+                    <Text>Furnishing Status</Text>
+                    <Text fontWeight="bold">{furnishingStatus}</Text>
+                    </Flex>
+                   )}
+               </Flex>
+               <Box>
+                   {amenities.length && <Text
+                   fontSize="2xl"
+                   fontWeight="bold"
+                   marginTop="5"
+                   >Amenities
+                   </Text>}
+                   <Flex flexWrap="wrap">
+                        {amenities.map((item) =>(
+                            item.amenities.map((amenity) =>(
+                                <Text key={amenity.text}>
+                                    {amenity.text}
+                                </Text>
+                               ))
+                        ))}
+                   </Flex>
+               </Box>
+   </Box>
   </Box>
 );
 
